6970fa4e6d642073355b8bb4b03b694bb47f3230
YoussefAhmed100 <141065629+YoussefAhmed100@users.noreply.github.com>
2024-11-12 15:01:36 +0200

    add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import { setupSwagger } from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
   }));
   app.enableCors(); 
   app.use(compression());
+//  setup swagger
+  setupSwagger(app);
+
   await app.listen(process.env.PORT||3000);
 }
 bootstrap();
