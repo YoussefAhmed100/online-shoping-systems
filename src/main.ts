398420b1c9e91e60f6de815ebe5fd35ccff5d187
YoussefAhmed100 <141065629+YoussefAhmed100@users.noreply.github.com>
2024-11-12 04:16:44 +0200

    update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }));
   app.enableCors(); 
   app.use(compression());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT||3000);
 }
 bootstrap();
 
